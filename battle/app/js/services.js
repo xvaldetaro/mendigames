68ea203524d424957b81030db11dab44f7a26e0c
Alexandre Valdetaro <xvaldetaro@gmail.com>
2013-07-11 22:27:40 -0300

    Sync

--- battle/app/js/services.js
+++ battle/app/js/services.js
@@ -5,4 +5,8 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('battle.services', []);
+angular.module('battle.services', ['ngResource']).
+    factory('Character', function($resource) {
+        return $resource('/battle/character/:characterId',
+            {format:'json', characterId:'@id'});
+    });
--- battle/tests/unit/servicesSpec.js
+++ battle/tests/unit/servicesSpec.js
@@ -1,3 +1,25 @@
 'use strict';
 
+describe('Battle controllers', function(){
 
+    describe('Campaign List Controller', function(){
+        var $httpBackend;
+
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('/battle/campaign/.json').
+                respond(_campaign_list_data);
+
+            scope = $rootScope.$new();
+            ctrl = $controller('CampaignListCtrl', {$scope:scope});
+        }));
+
+
+        it('should return a campaign_list model', function() {
+            expect(scope.campaign_list).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.campaign_list).toEqual(_results);
+        });
+    });
+});
