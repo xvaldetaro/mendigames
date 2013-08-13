75d106360ed7a02dd1c0605bee5599918ee9b850
ubuntu <xande@ubuntu.(none)>
2013-08-13 17:06:13 -0300

    Fixed production

--- mendigames/settings/production.py
+++ mendigames/settings/production.py
@@ -2,17 +2,3 @@ import os
 
 DEBUG = False
 
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-EMAIL_HOST= 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'caiubem'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-STATIC_URL = 'http://'+AWS_STORAGE_BUCKET_NAME+'s3.amazonaws.com/'
