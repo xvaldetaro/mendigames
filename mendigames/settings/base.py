1ae687125cb8e8733abd806d1d228988873bd99d
xande <xande@ubuntu2.(none)>
2013-08-13 17:49:53 -0300

    Static root

--- mendigames/settings/base.py
+++ mendigames/settings/base.py
@@ -67,7 +67,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '%s/static/' % APP_DIR
+STATIC_ROOT = os.environ["STATICS_ROOT"]
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
